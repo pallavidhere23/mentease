530c6ea7c0d48e8d261ce2ec1748956c3be0fee3
Pallavi Dhere <pallavidhere23@gmail.com>
2023-01-17 21:40:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Signinup from './Components/Signinup';
+
+
+function App() {
+  return (
+   <>
+     <Signinup/>
+   </>
+  );
+}
+
+export default App;
